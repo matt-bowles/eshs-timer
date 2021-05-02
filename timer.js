1fd1e6775882bbf042199490d58732e62d8b7409
matt-bowles <mattbowles1337@gmail.com>
2021-05-02 22:31:06 +1000

    Added value indicators (bubbles) to range inputs

--- timer.js
+++ timer.js
@@ -18,18 +18,51 @@ window.onload = () => {
     var startingTimeCtrl = document.querySelector("#startingTimeCtrl");
     var volumeCtrl = document.querySelector("#volumeCtrl");
 
-    intervalCtrl.addEventListener("change", (e) => {
+    intervalCtrl.addEventListener("input", (e) => {
         interval = e.target.value;
     });
 
-    startingTimeCtrl.addEventListener("change", (e) => {
+    startingTimeCtrl.addEventListener("input", (e) => {
         startingTime = e.target.value;
         document.querySelector("#startButton").disabled = false;
 
         // TODO - prevent user from selecting an invalid time (e.g. "before now")
     });
 
-    volumeCtrl.addEventListener("change", (e) => {
+    volumeCtrl.addEventListener("input", (e) => {
         bell.volume = e.target.value;
     });
+
+    document.querySelectorAll(".range-wrap").forEach((rangeWrap) => {
+        const range = rangeWrap.querySelector(".range");
+        const bubble = rangeWrap.querySelector(".bubble");
+
+        // Reposition bubble on input change
+        range.addEventListener("input", (e) => {
+            setBubblePos(range, bubble);
+        });
+
+        // Show bubbles on initial pageload
+        setBubblePos(range, bubble);
+    });
+}
+
+
+
+function setBubblePos(range, bubble) {
+    var bubbleText = range.value;   // Fallback
+
+    if (range.name == "intervalCtrl") {
+        bubbleText = `${range.value} mins`;
+    }
+    
+    else if (range.name == "volumeCtrl") {
+        bubbleText = `${range.value*100}%`
+    }
+    
+    bubble.innerHTML = bubbleText;
+
+    // Reposition bubble to be above the slider button
+    var newVal =((range.value - range.min) * 100) / (range.max - range.min);
+    bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
 }
