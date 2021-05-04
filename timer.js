d745ef6f56a785cae86ab6c699885065e293a566
matt-bowles <mattbowles1337@gmail.com>
2021-05-04 13:12:31 +1000

    Implemented main timer logic

--- timer.js
+++ timer.js
@@ -1,10 +1,134 @@
-var interval, startingTime;
+var interval = 10;
+var startingTime;
+var sessStart, sessEnd;
 var bell = new Audio("./media/bell.mp3");
- 
+
+var i = 0;
+
+/**
+ * The core logic loop of the program.
+ */
 function start() {
     // Hide configuration menu and show timer
     document.querySelector("#configContainer").style.visibility = "hidden";
     document.querySelector("#timerContainer").removeAttribute("hidden");
+
+    var startingTimeHour = startingTime.split(":")[0];
+	var startingTimeMin = startingTime.split(":")[1];
+
+    // Time formatting and padding bullshit
+    setStartingTimeText();
+
+    var date = new Date();
+    var msToStart = new Date(date.getFullYear(), date.getMonth(), date.getDate(), startingTimeHour, startingTimeMin, 0, 0) - Date.now();
+
+    // Update current time clock every second
+    setInterval(() => {
+        var now = new Date();
+        document.querySelector("#currentTime").textContent = `${pad(downTo2(now.getHours()))}:${pad(now.getMinutes())}:${pad(now.getSeconds())}`
+    }, 100);
+
+    setTimeout(() => {
+
+        sessEnd = addMins(interval);
+
+        playBell();
+        newSession(interval);
+
+        // Get rid of the starting at "hh:mm" text
+        document.querySelector("#startingTime").parentNode.style.visibility = "hidden";
+
+    }, msToStart);
+}
+
+/**
+ * Starts a new "session"
+ *  - plays the bell sound
+ *  - begins a new session
+ *  - updates the current session text on-screen
+ */
+function newSession() {
+    
+    sessStart = addMins(i*interval);
+    sessEnd = addMins((i+1)*interval);
+
+    document.querySelector("#sessStart").textContent = sessStart;
+    document.querySelector("#sessEnd").textContent = sessEnd;
+
+    i += 1;
+
+    setTimeout(() => {
+        playBell();
+        newSession();
+    }, interval*60*1000);
+}
+
+/**
+ * Adds a specified number of minutes to the starting time and then returns the result ().
+ * @param {*} numMins The number of minutes to be added to the starting time.
+ * @returns 
+ */
+function addMins(numMins) {
+    var stHrs = Number(startingTime.substr(0, 2));
+    var stMins = Number(startingTime.substr(3, 2));
+
+    stMins += numMins;
+
+    while (stMins >= 60) {
+        stHrs = Number(stHrs) + 1;
+        stMins -= 60;
+    }
+
+    if (stMins == 60) {
+        stMins = 0;
+    }
+
+    return `${pad(downTo2(stHrs))}:${pad(stMins)}`;
+}
+
+/**
+ * Pads a number with a leading 0, so that it is 2 digits.
+ * @param {*} num a value representing a minute or an hour
+ * @returns String
+ */
+function pad(num) {
+    if (num < 10) {
+        return `0${num}`
+    } else {
+        return String(num);
+    }
+}
+
+/**
+ * Reformats an hour value to not exceed 12 - e.g. 14 --> 2
+ * @param {*} hr Hour value (ranging from 00 - 23)
+ * @returns 
+ */
+function downTo2(hr) {
+    if (hr > 12) {
+        return String(hr-12);
+    } else {
+        return hr;
+    }
+}
+
+function setStartingTimeText() {
+    if (Number(startingTime.substr(0, 2)) > 12) {
+        var temp = String(Number(startingTime.substr(0, 2) - 12));
+
+        // TODO: Needs AM, too
+        startingTime = `${temp}:${startingTime.substr(3, 5)} PM`;
+
+        if ((startingTime.substr(0, 2)).includes(":")) {
+            startingTime = pad(startingTime);
+        }
+    } else if (startingTime.substr(0, 2) == 12) {
+        startingTime += " PM";
+    } else {
+        startingTime += " AM";
+    }
+
+    document.querySelector("#startingTime").textContent = startingTime;
 }
 
 function playBell() {
